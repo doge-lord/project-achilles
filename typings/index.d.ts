985af46fb11e5920ddd8d4acddeeeef0e5519ebc
Doge Lord <doge.lord25@gmail.com>
2016-09-19 03:34:47 +0800

    Initial commit

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,2 @@
 /// <reference path="globals/core-js/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/socket.io-client/index.d.ts" />
